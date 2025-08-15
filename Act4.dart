16fc5d966212f6a108f02778a1b9f0ada86628f3
Trisha Nicole Almario <trishanicolealmario04@gmail.com>
2025-08-15 17:48:24 +0800

    DartInputString

---
+++ Act4.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+    print("Trisha");
+    String? name = stdin.readLinesync();
+    print("Trisha ${name}");
+
+  if(name.isNotEmpty) {
+    print("Registration Successful");
+  } else {
+    print print("Trisha");
+  }
+}
